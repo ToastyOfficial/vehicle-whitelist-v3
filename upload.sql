411ce1ec51573c6d3e2cf56f4e5248f16937cb1b
ToastyOfficial <72546188+ToastyOfficial@users.noreply.github.com>
2025-06-14 16:15:26 -0400

    Create upload.sql
    Added missing SQL table

---
+++ upload.sql
@@ -0,0 +1,6 @@
+CREATE TABLE trusted (
+    discord varchar(255) DEFAULT NULL, 
+    spawncode varchar(255) DEFAULT NULL, 
+    owner varchar(255) DEFAULT NULL, 
+    name varchar(255) DEFAULT NULL
+);
