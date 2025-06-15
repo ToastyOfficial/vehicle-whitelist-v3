c00a255f0f0b662fa300c848154cb483fb1b4c08
ToastyOfficial <72546188+ToastyOfficial@users.noreply.github.com>
2025-06-14 23:47:33 -0400

    Update upload.sql
    - Updated tables

--- upload.sql
+++ upload.sql
@@ -2,5 +2,6 @@ CREATE TABLE trusted (
     discord varchar(255) DEFAULT NULL, 
     spawncode varchar(255) DEFAULT NULL, 
     owner varchar(255) DEFAULT NULL, 
+    vehname varchar(255) DEFAULT NULL,
     hash varchar(255) DEFAULT NULL
 );
